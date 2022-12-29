2dba377bee51aebd9b0b31cc940906d467976ae7
mykamins <mykamins@gmail.com>
2022-12-29 11:59:30 +0200

    Update FrequentExpenses.py

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 
-spending_categories = list()
+spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
